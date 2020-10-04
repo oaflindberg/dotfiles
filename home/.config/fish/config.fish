16e4a5dc438d02a5e9164811893756f032ef4139
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-10-04 16:12:54 +0200

    Updated fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,35 +1,36 @@
 alias ...="cd ../.."
 
-alias cat="bat"
-alias g="git"
-alias h="history"
-alias l="ls -1a"
 alias c="clear"
-alias tree="tree -I node_modules/ -F"
-alias fix="git diff --name-only | uniq | xargs code"
-alias ydl="youtube-dl"
-alias et="empty-trash"
-alias iopt="imageoptim -a"
-alias gief="gimmeh index.html style.css script.js"
-alias home="~"
-alias svtlist="svtplay-dl --list-quality"
-alias npmlistg="npmL -g --depth=0"
-alias space="du -sh *"
+alias cat="bat"
 alias diskspace="df -lH"
-alias react-app="npx create-react-app ."
-alias next-app="npx create-next-app"
+alias et="empty-trash"
+alias fix="git diff --name-only | uniq | xargs code"
+alias g="git"
 alias gaa="git add -A"
 alias gcmsg="git commit -m"
+alias gief="gimmeh index.html style.css script.js"
+alias gl="git pull"
 alias gp="git push"
 alias gst="git status"
+alias gsw="git switch"
+alias h="history"
+alias home="~"
+alias iopt="imageoptim -a"
+alias l="ls -1a"
+alias next-app="npx create-next-app"
+alias npmlistg="npmL -g --depth=0"
+alias react-app="npx create-react-app ."
+alias space="du -sh *"
+alias svtlist="svtplay-dl --list-quality"
+alias tree="tree -I node_modules/ -F"
+alias ydl="youtube-dl"
 
 set -x EDITOR "vim"
 
-set -x PATH "/usr/local/sbin" $PATH
-set -x PATH "/usr/local/share/npm/bin" $PATH
-set -x PATH "$HOME/.composer/vendor/bin" $PATH
-set -x PATH "usr/local/opt/mysql@5.7/bin" $PATH
+export PATH="/usr/local/opt/node@12/bin:$PATH"
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
 
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
+starship init fish | source
